9f924cf698f269e6066c1f22ff3484a02de11aac
cp735583410 <44771291+cp735583410@users.noreply.github.com>
2018-11-08 21:26:21 +0800

    2018.11.8练习

---
+++ Lianxi.java
@@ -0,0 +1,667 @@
+/**
+package p1;
+//主类
+public class Lianxi{
+	//主函数
+	public static void main(String[]args){
+		//假设全部买成公鸡，可以买33只；
+		for(int a=0;a<=33;a++){
+			//假设全部买成母鸡，可以买50只；
+			for(int b=0;b<=50;b++){
+				//假设全部买成小鸡，可以买300只；
+				for(int c=0;c<=300;c++){
+				//必须保证买小鸡花的钱为整数；
+					if((a+b+c==100&&3*a+2*b+c/3==100)&&c%3==0){
+						System.out.println(a+" "+b+" "+c);
+					}
+				}
+			}
+		}
+	}
+}
+
+package p1;
+//主类
+public class Lianxi{
+	//主函数
+	public static void main(String[]args){
+		//假设一次全部让男人搬运完。
+		for(int a=0;a<=9;a++){
+			//假设全部让女人一次搬运完
+			for(int b=0;b<=12;b++){
+				//假设一次让小孩搬运完
+				for(int c=0;c<=72;c++){
+					if(4*a+3*b+c/2==36&&a+b+c==36&&c%2==0){
+						System.out.println(a+" "+b+" "+c);
+					}
+				}
+			}
+		}
+	}
+}
+
+package p1;
+//主类
+public class Lianxi{
+	//主函数
+	public static void main(String[]args){
+		for(int n=100;n<=999;n++){
+			for(int a=n%10;a<10;a++){
+				for(int b=n/10%10;b<10;b++){
+					for(int c=n/100;b<10;b++){
+						if((a*a*a+b*b*b+c*c*c==n&&a==n%10)&&(b==n/10%10&&c==n/100)){
+							System.out.println(n);
+						}
+					}
+				}
+			}
+		}
+	}
+}
+package p1;
+public class Lianxi{
+    public static void main(String[]args){
+		for(int a=0;a<=33;a++){
+			for(int b=0;b<=50;b++){
+				int c=100-a-b;
+				if(a+b+c==100&&3*a+2*b+c/3==100&&c%3==0){
+					System.out.println(a+" "+b+" "+c);
+				}
+			}
+		}
+	}
+}
+
+package p1;
+public class Lianxi{
+	public static void main(String[]args){
+		for(int a=0;a<=33;a++){
+			for(int b=0;b<=50;b++){
+				int c=100-a-b;
+				if(a+b+c==100&&3*a+2*b+c/3==100&&c%3==0){
+					System.out.println(a+" "+b+" "+c);
+				}
+			}
+		}
+	}
+}
+
+package p1;
+public class Lianxi{
+	public static void main(String[]args){
+		for(int a=0;a<=33;a++){
+			for(int b=0;b<=50;b++){
+				int c=100-a-b;
+				if((a+b+c==100&&3*a+2*b+c/3==100)&&c%3==0){
+					System.out.println(a+" "+b+" "+c);
+				}
+			}
+		}
+	}
+}
+
+package p1;
+public class Lianxi{
+	public static void main(String[]args){
+		for(int a=0;a<=33;a++){
+			for(int b=0;b<=50;b++){
+				int c=100-a-b;
+				if((a+b+c==100&&3*a+2*b+c/3==100)&&c%3==0){
+					System.out.println(a+" "+b+" "+c);
+				}
+			}
+		}
+	}
+}
+
+package p1;
+public class Lianxi{
+	public static void main(String[]args){
+		for(int a=0;a<=33;a++){
+			for(int b=0;b<=50;b++){
+				int c=100-a-b;
+				if(a+b+c==100&&3*a+2*b+c/3==100&&c%3==0){
+					System.out.println(a+" "+b+" "+c);
+				}
+			}
+		}
+	}
+}
+
+package p1;
+public class Lianxi{
+	public static void main(String[]args){
+		for(int a=0;a<=33;a++){
+			for(int b=0;b<=50;b++){
+				int c=100-a-b;
+				if((a+b+c==100&&3*a+2*b+c/3==100)&&c%3==0){
+					System.out.println(a+" "+b+" "+c);
+				}
+			}
+		}
+	}
+}
+
+package p1;
+public class Lianxi{
+	public static void main(String[]args){
+		for(int a=0;a<=33;a++){
+			for(int b=0;b<=50;b++){
+				int c=100-a-b;
+				if((a+b+c==100&&3*a+2*b+c/3==100)&&c%3==0){
+					System.out.println(a+" "+b+" "+c);
+				}
+			}
+		}
+	}
+}
+
+package p1;
+public class Lianxi{
+	public static void main(String[]args){
+		for(int a=0;a<=33;a++){
+			for(int b=0;b<=50;b++){
+				int c=100-a-b;
+				if((a+b+c==100&&3*a+2*b+c/3==100)&&c%3==0){
+					System.out.println(a+" "+b+" "+c);
+				}
+			}
+		}
+	}
+}
+
+package p1;
+public class Lianxi{
+	public static void main(String[]args){
+		for(int a=0;a<=33;a++){
+			for(int b=0;b<=50;b++){
+				int c=100-a-b;
+				if((a+b+c==100&&3*a+2*b+c/3==100)&&c%3==0){
+					System.out.println(a+" "+b+" "+c);
+				}
+			}
+		}
+	}
+}
+
+package p1
+//主类
+public class Lianxi{
+	//主函数
+	public static void main(String[]args){
+		//假设只需要男人搬运，则最多需要9人；
+		for(int a=0;a<=9;a++){
+			//假设只需要女人搬运，则最多需要12人；
+			for(int b=0;b<=12;b++){
+				//假设只需要小孩搬运，则需要最多72个小孩；
+				for(int c=0;c<=72;c++){
+					if(4*a+3*b+c/2==36&&a+b+c==36&&c%2==0){
+						System.out.println(a+" "+b+" "+c);
+					}
+				}
+			}
+		}
+	}
+}
+
+package p1;
+//主类
+public class Lianxi{
+	//主函数
+	public static void main(String[]args){
+		//假设只需要男人搬运，则最多需要9人；
+		for(int a=0;a<=9;a++){
+			//假设只需要女人搬运，则最多需要12人；
+			for(int b=0;b<=12;b++){
+				//假设只需要小孩搬运，则需要最多72个小孩；
+				for(int c=0;c<=72;c++){
+					if(4*a+3*b+c/2==36&&a+b+c==36&&c%2==0){
+						System.out.println(a+" "+b+" "+c);
+					}
+				}
+			}
+		}
+	}
+}
+
+package p1;
+//主类
+public class Lianxi{
+	//主函数
+	public static void main(String[]args){
+		//假设只需要男人搬运，则最多需要9人；
+		for(int a=0;a<=9;a++){
+			//假设只需要女人搬运，则最多需要12人；
+			for(int b=0;b<=12;b++){
+				//假设只需要小孩搬运，则需要最多72个小孩；
+				for(int c=0;c<=72;c++){
+					if(4*a+3*b+c/2==36&&a+b+c==36&&c%2==0){
+						System.out.println(a+" "+b+" "+c);
+					}
+				}
+			}
+		}
+	}
+}
+
+package p1;
+//主类
+public class Lianxi{
+	//主函数
+	public static void main(String[]args){
+		//假设只需要男人搬运，则最多需要9人；
+		for(int a=0;a<=9;a++){
+			//假设只需要女人搬运，则最多需要12人；
+			for(int b=0;b<=12;b++){
+				//假设只需要小孩搬运，则需要最多72个小孩；
+				for(int c=0;c<=72;c++){
+					if(4*a+3*b+c/2==36&&a+b+c==36&&c%2==0){
+						System.out.println(a+" "+b+" "+c);
+					}
+				}
+			}
+		}
+	}
+}
+
+package p1;
+//主类
+public class Lianxi{
+	//主函数
+	public static void main(String[]args){
+		//假设只需要男人搬运，则最多需要9人；
+		for(int a=0;a<=9;a++){
+	        //假设只需要女人搬运，则最多需要12人；
+			for(int b=0;b<=12;b++){
+				//假设只需要小孩搬运，则需要最多72个小孩；
+				for(int c=0;c<=72;c++){
+					if(4*a+3*b+c/2==36&&a+b+c==36&&c%2==0){
+						System.out.println(a+" "+b+" "+c);
+					}
+				}
+			}
+		}
+	}
+}
+
+package p1;
+//主类
+public class Lianxi{
+	//主函数
+	public static void main(String[]args){
+		//假设只需要男人搬运，则最多需要9人；
+		for(int a=0;a<=9;a++){
+			//假设只需要女人搬运，则最多需要12人；
+			for(int b=0;b<=12;b++){
+				//假设只需要小孩搬运，则需要最多72个小孩；
+				for(int c=0;c<=72;c++){
+					if(4*a+3*b+c/2==36&&a+b+c==36&&c%2==0){
+						System.out.println(a+" "+b+" "+c);
+					}
+				}
+			}
+		}
+	}
+}
+package p1;
+public class Lianxi{
+	public static void main(Stirng[]args){
+		for(int a=0;a<=33;a++){
+			for(int b=0;b<=50;b++){
+				int c=100-a-b;
+				if((a+b+c==100&&3*a+2*b+c/3==100)&&c%3==0){
+					System.out.println(a+" "+b+" "+c);
+				}
+			}
+		}
+	}
+}
+package p1;
+public class Lianxi{
+	public static void main(String[]args){
+		for(int a=0;a<=33;a++){
+			for(int b=0;b<=50;b++){
+				int c=100-a-b;
+				if((a+b+c==100&&3*a+2*b+c/3==100)&&c%3==0){
+					System.out.println(a+" "+b+" "+c);
+				}
+			}
+		}
+	}
+}
+package p1;
+public class Lianxi{
+	public static void main(String[]args){
+		for(int a=0;a<=33;a++){
+			for(int b=0;b<=50;b++){
+				int c=100-a-b;
+				if(a+b+c==100&&3*a+2*b+c/3==100&&c%3==0){
+					System.out.println(a+" "+b+" "+c);
+				}
+			}
+		}
+	}
+}
+package p1;
+public class Lianxi{
+	public static void main(String[]args){
+		for(int a=0;a<=33;a++){
+			for(int b=0;b<=50;b++){
+				int c=100-a-b;
+				if(a+b+c==100&&3*a+2*b+c/3==100&&c%3==0){
+					System.out.println(a+" "+b+" "+c);
+				}
+			}
+		}
+	}
+}
+package p1;
+public class Lianxi{
+	public static void main(String[]args){
+		for(int a=0;a<=9;a++){
+			for(int b=0;b<=12;b++){
+				for(int c=0;c<=72;c++){
+					if(4*a+3*b+c/2==36&&a+b+c==36&&c%2==0){
+						System.out.println(a+" "+b+" "+c);
+					}
+				}
+			}
+		}
+	}
+}
+package p1;
+public class Lianxi{
+	public static void main(String[]args){
+		for(int a=0;a<=9;a++){
+			for(int b=0;b<=12;b++){
+				for(int c=0;c<=72;c++){
+					if(4*a+3*b+c/2==36&&a+b+c==36&&c%2==0){
+						System.out.println(a+" "+b+" "+c);
+					}
+				}
+			}
+		}
+	}
+}
+package p1;
+public class Lianxi{
+	public static void main(String[]args){
+		for(int a=0;a<=9;a++){
+			for(int b=0;b<=12;b++){
+				for(int c=0;c<=72;c++){
+					if(4*a+3*b+c/2==36&&a+b+c==36&&c%2==0){
+						System.out.println(a+" "+b+" "+c);
+					}
+				}
+			}
+		}
+	}
+}
+package p1;
+public class Lianxi{
+	public static void main(String[]args){
+		for(int a=0;a<=9;a++){
+			for(int b=0;b<=12;b++){
+				for(int c=0;c<=72;c++){
+					if(4*a+3*b+c/2==36&&a+b+c==36&&c%2==0){
+						System.out.println(a+" "+b+" "+c);
+					}
+				}
+			}
+		}
+	}
+}
+package p1;
+public class Lianxi{
+	public static void main(String[]args){
+		for(int a=0;a<=9;a++){
+			for(int b=0;b<=12;b++){
+				for(int c=0;c<=72;c++){
+					if(4*a+3*b+c/2==36&&a+b+c==36&&c%2==0){
+						System.out.println(a+" "+b+" "+c);
+					}
+				}
+			}
+		}
+	}
+}
+package p1;
+public class Lianxi{
+	public static void main(String[]args){
+		for(int a=0;a<=9;a++){
+			for(int b=0;b<=12;b++){
+				for(int c=0;c<=72;c++){
+					if(4*a+3*b+c/2==36&&a+b+c==36&&c%2==0){
+						System.out.println(a+" "+b+" "+c);
+					}
+				}
+			}
+		}
+	}
+}
+package p1;
+public class Lianxi{
+	public static void main(String[]args){
+		for(int a=0;a<=33;a++){
+			for(int b=0;b<=50;b++){
+				int c=100-a-b;
+				if((3*a+2*b+c/3==100&&a+b+c==100)&&c%3==0){
+					System.out.println(a+" "+b+" "+c);
+				}
+			}
+		}
+	}
+}
+package p1;
+public class Lianxi{
+	public static void main(String[]args){
+		for(int a=0;a<=9;a++){
+			for(int b=0;b<=12;b++){
+				for(int c=0;c<=72;c++){
+					if(4*a+3*b+c/2==36&&a+b+c==36&&c%2==0){
+						System.out.println(a+" "+b+" "+c);
+					}
+				}
+			}
+		}
+	}
+}
+
+package p1;
+public class Lianxi{
+	public static void main(String[]args){
+		for(int a=0;a<=33;a++){
+			for(int b=0;b<=50;b++){
+				int c=100-a-b;
+				if((3*a+2*b+c/3==100&&a+b+c==100)&&c%3==0){
+					System.out.println(a+" "+b+" "+c);
+				}
+			}
+		}
+	}
+}
+package p1;
+public class Lianxi{
+	public static void main(String[]args){
+		for(int a=0;a<=33;a++){
+			for(int b=0;b<=50;b++){
+				int c=100-a-b;
+				if((a+b+c==100&&3*a+2*b+c/3==100)&&c%3==0){
+					System.out.println(a+" "+b+" "+c);
+				}
+			}
+		}
+	}
+}
+
+package p1;
+public class Lianxi{
+	public static void main(String[]args){
+		for(int n=100;n<1000;n++){
+			for(int a=0;a<10;a++){
+				for(int b=0;b<10;b++){
+					for(int c=1;c<10;c++){
+						if((a*a*a+b*b*b+c*c*c==n&&a==n%10)&&(b==n/10%10&&c==n/100)){
+							System.out.println(n);
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+package p1;
+public class Lianxi{
+	public static void main(String[]args){
+		int sum=0;
+		for(int i=1;i<=100;i++){
+			sum+=i;
+		}
+		System.out.println(sum);
+	}
+}
+
+package p1;
+public class Lianxi{
+	public static void main(String[]args){
+		for(int a=0;a<=33;a++){
+			for(int b=0;b<=50;b++){
+				int c=100-a-b;
+				if((a+b+c==100&&3*a+2*b+c/3==100)&&c%3==0){
+					System.out.println(a+" "+b+" "+c);
+				}
+			}
+		}
+	}
+}
+
+package p1;
+public class Lianxi{
+	public static void main(String[]args){
+		for(int a=0;a<=33;a++){
+			for(int b=0;b<=50;b++){
+				int c=100-a-b;
+				if((a+b+c==100&&3*a+2*b+c/3==100)&&c%3==0){
+					System.out.println(a+" "+b+" "+c);
+				}
+			}
+		}
+	}
+}
+
+package p1;
+public class Lianxi{
+	public static void main(String[]args){
+		for(int i=1;i<=100;i++){
+			if(i%7!=0&&i%10!=7&&i/10%7!=7){
+				System.out.println(i);
+			}
+		}
+	}
+}
+package p1;
+public class Lianxi{
+	public static void main(String[]args){
+		for(int a=0;a<=9;a++){
+			for(int b=0;b<=12;b++){
+				int c=36-a-b;
+				if(a+b+c==36&&4*a+3*b+c/2==36&&c%2==0){
+					System.out.println(a+" "+b+" "+c);
+				}	
+			}
+		}
+	}
+}
+package p1;
+public class Lianxi{
+	public static void main(String[]args){
+		for(int a=0;a<=9;a++){
+			for(int b=0;b<=12;b++){
+				int c=36-a-b;
+				if(a+b+c==36&&4*a+3*b+c/2==36&&c%2==0){
+					System.out.println(a+" "+b+" "+c);
+				}
+			}
+		}
+	}
+}
+package p1;
+public class Lianxi{
+	public static void main(String[]args){
+		for(int a=0;a<=33;a++){
+			for(int b=0;b<=50;b++){
+				int c=100-a-b;
+				if(a+b+c==100&&3*a+2*b+c/3==100&&c%3==0){
+					System.out.println(a+" "+b+" "+c);
+				}
+			}
+		}
+	}
+}
+package p1;
+public class Lianxi{
+	public static void main(String[]args){
+		for(int a=0;a<=9;a++){
+			for(int b=0;b<=12;b++){
+				int c=36-a-b;
+				if(a+b+c==36&&4*a+3*b+c/2==36&&c%2==0){
+					System.out.println(a+" "+b+" "+c);
+				}
+			}
+		}
+	}
+}
+package p1;
+public class Lianxi{
+	public static void main(String[]args){
+		for(int a=0;a<=33;a++){
+			for(int b=0;b<=50;b++){
+				int c=100-a-b;
+				if((a+b+c==100&&3*a+2*b+c/3==100)&&c%3==0){
+					System.out.println(a+" "+b+" "+c);
+				}
+			}
+		}
+    
+	}
+}
+package p1;
+public class Lianxi{
+	public static void main(String[]args){
+		for(int a=0;a<=33;a++){
+			for(int b=0;b<=50;b++){
+				int c=100-a-b;
+				if((a+b+c==100&&3*a+2*b+c/3)&&c%3==0){
+					System.out.println(a+" "+b+" "+c);
+				}
+			}
+		}
+	}
+}*/
+package p1;
+public class Lianxi{
+	public static void main(String[]args){
+		for(int a=0;a<=33;a++){
+			for(int b=0;b<=50;b++){
+				int c=100-a-b;
+				if(a+b+c==100&&3*a+2*b+c/3==100&&c%3==0){
+					System.out.println(a+" "+b+" "+c);
+				}
+			}
+		}
+	}
+}
+package p1;
+public class Lianxi{
+	public static void main(String[]args){
+		for(int a=0;a<=9;a++){
+			for(int b=0;b<=12;b++){
+				int c=36-a-b;
+				if(a+b+c==36&&4*a+3*b+c/2==36&&c%2==0){
+					System.out.println(a+" "+b+" "+c);
+				}
+			}
+		}
+	}
+}
