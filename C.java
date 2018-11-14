8b069c93349fa1694910f2dc3a713a93303f936a
cp735583410 <44771291+cp735583410@users.noreply.github.com>
2018-11-14 21:26:15 +0800

    2018.11.14练习

---
+++ C.java
@@ -0,0 +1,253 @@
+
+package p1;
+public class C{
+	public static void main(String[]args){
+		if(a(23)){
+			System.out.println("是质数");
+		}
+		else {
+			System.out.println("不是质数");
+		}
+	}
+	//写一个函数 定义一个整形的参数
+	public static boolean a(int n){
+		int sum=0;
+		for(int i=1;i<=n;i++){
+			if(n%i==0){
+				sum++;//当n%i=0成立时，sum++, 计算n的因子数量
+			}
+		}
+		if(sum>2){//当N的因子数量大于2时，n不是质数，反之，n 为质数。
+			return false;
+		}
+		else return true;
+	}
+}
+
+package p1;
+public class C{
+	public static void main(String[]args){
+		if(a(13)){
+			System.out.println("是质数");
+		}
+		else{
+			System.out.println("不是质数");
+		}
+	}//写一个函数 定义一个整形的参数
+	public static boolean a(int n){
+		int sum=0;
+		for(int i=1;i<=n;i++){
+			if(n%i==0){
+				sum++;//当n%i=0成立时，sum++, 计算n的因子数量
+			}
+		}
+		if(sum>2){//当N的因子数量大于2时，n不是质数，反之，n 为质数。
+			return false ;
+		}
+		else return true;
+	}
+}
+
+package p1;
+public class C{
+	public static void main(String[]args){
+		if(a(24)){
+			System.out.println("是质数");
+		}
+		else {
+			System.out.println("不是质数");
+		}
+	}//写一个函数 定义一个整形的参数
+	public static boolean a(int n){
+		int sum=0;
+		for(int i=1;i<=n;i++){
+			if(n%i==0){
+				sum++;//当n%i=0成立时，sum++, 计算n的因子数量
+			}
+		}
+		if(sum>2){//当N的因子数量大于2时，n不是质数，反之，n 为质数。
+			return false;
+		}
+		else return true;
+	}
+}
+
+package p1;
+public class C{
+	public static void main(String[]args){
+		if(a(23)){
+			System.out.println("是质数");
+		}
+		else{
+			System.out.println("不是质数");
+		}
+	}
+	//写一个函数 定义一个整形的参数
+	public static boolean a(int n){
+		int sum=0;
+		for(int i=1;i<=n;i++){
+			if(n%i==0){
+				sum++;//当n%i=0成立时，sum++, 计算n的因子数量
+			}
+		}
+		if(sum>2){//当N的因子数量大于2时，n不是质数，反之，n 为质数。
+			return false;
+			return true;
+		}
+	}
+}
+
+package p1;
+public class C{
+	public static void main(String[]args){
+		if(a(23)){
+			System.out.println("是质数");
+		}
+		else{
+			System.out.println("不是函数");
+		}
+	}
+	//写一个函数 定义一个整形的参数
+	public static boolean a(int n){
+		int sum=0;
+		for(int i=1;i<=n;i++){
+			if(n%i==0){
+				sum++;//当n%i=0成立时，sum++, 计算n的因子数量
+			}
+		}
+		if(sum>2){
+			//当N的因子数量大于2时，n不是质数，反之，n 为质数。
+			return false;			
+		}
+		else{
+			return true;
+		}
+	}
+}
+
+package p1;
+public class C{
+	public static void main(String[]args){
+		if(a(29)){
+			System.out.println("是质数");
+		}
+		else{
+			System.out.println("不是");
+		}
+	}
+	//写一个函数 定义一个整形的参数
+	public static boolean a(int n){
+		int sum=0;
+		for(int i=1;i<=n;i++){
+			if(n%i==0){
+				sum++;//当n%i=0成立时，sum++, 计算n的因子数量
+			}
+		}
+		if(sum>2){
+			//当N的因子数量大于2时，n不是质数，反之，n 为质数。
+			return false;
+		}
+		else return true;
+	}
+}
+
+package p1;
+public class C{
+	public static void main(String[]args){
+		if(a(37)){
+			System.out.println("是质数");
+		}
+		else{
+			System.out.println("不是质数");
+		}
+	}
+	//写一个函数 定义一个整形的参数
+	public static boolean a(int n){
+		int sum=0;
+		for(int i=1;i<=n;i++){
+			if(n%i==0){
+				sum++;//当n%i=0成立时，sum++, 计算n的因子数量
+			}
+		}
+		if(sum>2){
+			//当N的因子数量大于2时，n不是质数，反之，n 为质数。
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
+}
+package p1;
+public class C{
+    public static void main(String[]args){
+		if(a(67)){
+			System.out.println("是质数");
+		}
+	    else{
+			System.out.println("不是质数");
+		}
+	
+	////写一个函数 定义一个整形的参数
+	public static boolean a(int n){
+		int sum=0;
+		for(int i=1;i<=n;i++){
+			if(n%i==0){
+				sum++;//当n%i=0成立时，sum++, 计算n的因子数量
+			}
+		}
+		if(sum>2){
+			//当N的因子数量大于2时，n不是质数，反之，n 为质数。
+			return false;
+		}
+		else return true;
+	}
+}
+package p1;
+public class C{
+	public static void main(String[]args){
+		if(a(23)){
+			System.out.println("是质数");
+		}
+		else{
+			System.out.println("不是质数");
+		}
+	}
+	//写一个函数 定义一个整型的参数
+	public static boolean a(int n){
+		int sum=0;
+		for(int i=1;i<=n;i++){
+			if(n%i==0){
+				sum++;//当n%i=0成立时，sum++，计算n的因子数量
+			}
+		}
+		if(sum>2){
+			//当n的因子数量大于2时，n不是质数，反之，n为质数。
+			return false;
+		}
+		else return true;
+	}
+}
+package p1;
+public class C{
+	public static void main(String[]args){
+		if(a(23)){
+			System.out.println("不是质数");
+		}
+		else {
+			System.out.println("是质数");
+		}
+	}
+	public static boolean a(int n){
+		int sum=0;
+		for(int i=1;i<=n;i++){
+			if(n%i==0){
+				sum++;
+			}
+		}
+		if(sum>2){
+			return false;
+		}
+		else return true;
+	}
+}
